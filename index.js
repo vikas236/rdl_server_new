c9b25f9590457f1404a41693c393a8379e9588f7
vikas236 <saivikas461@gmail.com>
2024-06-02 14:47:00 +0530

    update psql url

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const { Pool } = require("pg");
 
+// const pool = new Pool({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+// });
+
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 const app = express();
